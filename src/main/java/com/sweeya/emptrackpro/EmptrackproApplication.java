f7591127404d61506a3f933c62f477db245e744d
Sweeya Sankuru <sweeyasankuru@Sweeyas-Air.lan>
2025-06-17 01:58:27 -0500

    Initial commit

---
+++ src/main/java/com/sweeya/emptrackpro/EmptrackproApplication.java
@@ -0,0 +1,13 @@
+package com.sweeya.emptrackpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptrackproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptrackproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sweeya/emptrackpro/EmptrackproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweeya.emptrackpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptrackproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
